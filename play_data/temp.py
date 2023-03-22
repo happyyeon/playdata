d3177420be594cb8583f491951e4273ed95ff624
happyyeon <happyeon@kakao.com>
2023-03-22 17:48:12 +0900

    KNN Algorithm

--- play_data/temp.py
+++ play_data/temp.py
@@ -1,17 +1,12 @@
-def convert(num,b):
-    answer = 0
+def convert(string):
     alpha_to_num = dict()
-
-    for i in range(65,b+55):
-        alpha_to_num[chr(i)] = i-55
+    for i in range(65,91):
+        alpha_to_num[chr(i)] = i-64
     
-    for i in range(len(num)):
-        try:
-            answer += int(num[i])*(b**(len(num)-1-i))
-        except:
-            answer += alpha_to_num[num[i]]*(b**(len(num)-1-i))
-    return answer
+    return number
 
 if __name__ == '__main__':
-    N,B = input().split()
-    print(convert(N,int(B)))
+    L = int(input())
+    string = input()
+    print(convert(string))
+    
