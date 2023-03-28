90279e40da63a4d85025dbaed2fde5edfa83db23
happyyeon <happyeon@kakao.com>
2023-03-28 16:48:00 +0900

    20230328 오후 수업 내용

--- test.py
+++ test.py
@@ -1,30 +1,11 @@
-import sys
-from collections import defaultdict
-sys.setrecursionlimit(int(1e6))
-input = sys.stdin.readline
+"""
+jupyter notebook과 mysql 이미지를 제외하고 모든 이미지 제거하는 코드
+"""
 
-def init():
-    N = int(input())
-    graph = defaultdict(list)
-
-    for _ in range(N-1):
-        a,b = map(int,input().split())
-        graph[a].append(b)
-        graph[b].append(a)
-    return N,graph
-
-def find_parent(graph,root):
-    cur = root
-    for next in graph[cur]:
-        if not parent_list[next]:
-            parent_list[next] = cur
-            find_parent(graph,next)
-    return
-
-if __name__ == '__main__':
-    N, graph = init()
-    parent_list = [0]*(N+1)
-    parent_list[1] = 1
-    find_parent(graph,1)
-    for x in parent_list[2:]:
-        print(x)
+import subprocess
+text = subprocess.check_output("docker images",shell=True).decode()
+except_set = {'jupyter/datascience-notebook','mysql'}
+for x in text.split('\n')[1:-1]:
+    temp = x.split()
+    if temp[0] not in except_set:
+        subprocess.call("docker rmi {}".format(temp[2]), shell=True)
