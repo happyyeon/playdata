d4e84883c79679a7e7c89a005483ece5e461c405
happyyeon <happyeon@kakao.com>
2023-04-11 16:46:31 +0900

    20230411 Django 게시판 목록과 상세 페이지 작성

--- pybo/pybo/views.py
+++ pybo/pybo/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from django.http import HttpResponse
+from .models import Question
+from django.utils import timezone
 # Create your views here.
 def index(request):
-    return HttpResponse("4월 10일 월요일")
+    q_list = Question.objects.order_by('-create_date')
+    return render(request, 'pybo/question_list.html', {'question_list' : q_list})
+
+def detail(request,question_id):
+    q = get_object_or_404(Question, pk=question_id)
+    return render(request,'pybo/question_detail.html', {
+        'question' : q
+    })
+
+def answer_create(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
