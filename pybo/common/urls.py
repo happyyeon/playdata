5d045b56641d9b9cfb821d2e4a6a02a38020b6fa
happyyeon <happyeon@kakao.com>
2023-04-13 17:01:09 +0900

    20230413 장고 게시판 로그인 기능

--- pybo/common/urls.py
+++ pybo/common/urls.py
@@ -9,6 +9,7 @@ app_name = 'common'
 
 
 urlpatterns = [
-
-
+    path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
+    path('signup/', views.signup,name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- pybo/pybo/views.py
+++ pybo/pybo/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     q_list = Question.objects.order_by('-create_date')
@@ -14,17 +15,20 @@ def detail(request,question_id):
         'question' : q
     })
 
+@login_required(login_url='common:login')
 def answer_create(request,question_id):
     question = get_object_or_404(Question, pk=question_id)
     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     return redirect('pybo:detail', question_id=question.id)
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == "POST":
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
             question.create_date = timezone.now()
+            question.author = request.user
             question.save()
             return redirect('pybo:index')
     else:
