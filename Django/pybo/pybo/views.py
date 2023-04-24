10ab62af8fb687d9eacfd88234e8fc13676d8dcf
happyyeon <happyeon@kakao.com>
2023-04-24 16:45:11 +0900

    django board reply count implement

--- Django/pybo/pybo/views.py
+++ Django/pybo/pybo/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render,get_object_or_404,redirect
 from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
-from .forms import QuestionForm
+from .forms import QuestionForm,AnswerForm
 from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def index(request):
     q_list = Question.objects.order_by('-create_date')
@@ -15,11 +16,25 @@ def detail(request,question_id):
         'question' : q
     })
 
+
 @login_required(login_url='common:login')
-def answer_create(request,question_id):
+def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question.id)
+
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            redirect('pybo:detail', question_id = question.id)
+    else:
+        form = AnswerForm()
+    context = {'question' : question, 'form' : form}
+    
+    return render(request, 'pybo/question_detail.html', context)
 
 @login_required(login_url='common:login')
 def question_create(request):
